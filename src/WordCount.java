c652206b929975fd1f41b87478e4d4188d70d47a
maciekserwin <maciekserwin@gmail.com>
2023-06-29 23:39:20 +0200

    add while loop for reading and count file line

--- src/WordCount.java
+++ src/WordCount.java
@@ -5,5 +5,11 @@ public class WordCount {
     public static void main(String[] args) throws Exception {
         File file = new File("talesoftwocities.txt");
         Scanner scanner = new Scanner(file);
+
+        int words = 0;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            words += line.split(" ").length;
+        }
     }
 }
