6cdea24200c69b94048e4a921e5546d6bd679268
maciekserwin <maciekserwin@gmail.com>
2023-06-29 23:30:35 +0200

    add scanner object

--- src/WordCount.java
+++ src/WordCount.java
@@ -4,5 +4,6 @@ import java.util.Scanner;
 public class WordCount {
     public static void main(String[] args) throws Exception {
         File file = new File("talesoftwocities.txt");
+        Scanner scanner = new Scanner(file);
     }
 }
